91ee2651b7ddbcff87b7572fb5292e20a6d64b07
j-pai <jesse.pai@gmail.com>
2021-04-13 22:00:10 -0400

    Fixed bug with flatten operation where R was used twice.

--- cluster_painter.py
+++ cluster_painter.py
@@ -46,9 +46,8 @@ BLACK_LISTED_COLORS = {
 }
 
 class ClusterPainter:
-    def __init__(self):
+    def __init__(self, plot_enable, sim):
         screen_width, screen_height = pyautogui.size()
-        print("Screen Information: ", screen_width, screen_height)
         self.x_min = 0
         self.x_max = screen_width
         self.y_min = 0
@@ -57,7 +56,8 @@ class ClusterPainter:
         self.bottom_right_set = False
         self.submit_button = None
         self.last_coord = []
-
+        self.plot_enable = plot_enable or sim is not None
+        self.sim = sim
 
     def on_click(self, x, y, button, pressed):
         if self.top_left_set and self.bottom_right_set and not pressed:
@@ -68,37 +68,41 @@ class ClusterPainter:
                 self.last_coord = []
                 return False
         if not self.top_left_set and not pressed:
-            print("Top Left: {} {}".format(x, y))
+            print('Top Left: {} {}'.format(x, y))
             self.top_left_set = True
             self.x_min = x
             self.y_min = y
             return False
         elif not pressed:
-            print("Bottom Right: {} {}".format(x, y))
+            print('Bottom Right: {} {}'.format(x, y))
             self.bottom_right_set = True
             self.x_max = x
             self.y_max = y
             return False
 
     def set_bounderies(self):
-        print("SET BOUNDARY : top_left")
+        if self.sim:
+            return
+        print('SET BOUNDARY : top_left')
         with mouse.Listener(on_click=self.on_click) as listener:
             listener.join()
-        print("SET BOUNDARY : bottom_right")
+        print('SET BOUNDARY : bottom_right')
         with mouse.Listener(on_click=self.on_click) as listener:
             listener.join()
 
         if self.x_max < self.x_min or self.x_max < self.x_min:
             raise ValueError(
-                "bottom_right ({}, {}) is less than top_left ({}, {})".format(
+                'bottom_right ({}, {}) is less than top_left ({}, {})'.format(
                     self.x_min, self.y_min, self.x_max, self.y_max,))
 
     def set_submit_button(self):
-        print("SET CONTINUE BUTTON")
+        if self.sim:
+            return
+        print('SET CONTINUE BUTTON')
         submit_button = pyautogui.locateOnScreen(
             'ui_targets/submit_button.png', confidence=0.5)
         if not submit_button:
-            raise ValueError("[Submit] Button not found.")
+            raise ValueError('[Submit] Button not found.')
         self.submit_button = pyautogui.center(submit_button)
         print(self.submit_button)
 
@@ -109,9 +113,10 @@ class ClusterPainter:
             self.x_max - self.x_min,
             self.y_max - self.y_min,
         ))
-        # image_name = "cluster_{}.png".format(datetime.datetime.now().timestamp())
-        # image_path = "images/{}".format(image_name)
-        # image.save(image_path)
+        if self.plot_enable and not self.sim:
+            image_name = 'cluster_{}.png'.format(datetime.datetime.now().timestamp())
+            image_path = 'images/{}'.format(image_name)
+            image.save(image_path)
         return image
 
     def transform_cluster_coord(self, coord):
@@ -129,21 +134,25 @@ class ClusterPainter:
             r = (c >> 16) & 0xFF;
             g = (c >> 8) & 0xFF;
             b = c & 0xFF;
+
             if r == 0 and g == 0 and b == 0:
                 return 0
-            value = (r / 255 + 2) + (g / 255 + 1) + b / 255
-            return np.uint32(value / 3 * 100)
-        # with Image.open(self.screenshot()) as image:
-        # with Image.open("images\cluster_1618287905.126905.png") as image:
-            # converted_image = np.array(image.convert('RGB'))
-        converted_image = np.array(self.screenshot().convert('RGB'))
+
+            return b
+
+        converted_image = None
+        if self.sim:
+            #with Image.open(self.screenshot()) as image:
+            with Image.open(self.sim[0]) as image:
+                converted_image = np.array(image.convert('RGB'))
+        else:
+            converted_image = np.array(self.screenshot().convert('RGB'))
 
         cv_image_array = np.asarray(converted_image, dtype='uint32')
         cv_image_flatten = (cv_image_array[:, :, 0] << 16) \
             + (cv_image_array[:, :, 1] << 8) \
-            + (cv_image_array[:, :, 0])
+            + (cv_image_array[:, :, 2])
         cv_image_flatten = np.vectorize(ignore_color)(cv_image_flatten)
-
         cv_image_percentage = np.vectorize(heat_map_percentage)(cv_image_flatten)
 
         cluster_x = []
@@ -158,19 +167,22 @@ class ClusterPainter:
                     cluster_y.append(y)
                     cluster_weight.append(pct)
 
+        print("Done setting up data.")
+
         z = np.vstack((cluster_x, cluster_y)).T
         z = np.float32(z)
 
-        # plt.subplot(222)
-        # percentage_plt = plt.imshow(cv_image_percentage)
+        if self.plot_enable:
+            plt.subplot(222)
+            percentage_plt = plt.imshow(cv_image_percentage)
 
-        # plt.subplot(223)
-        # flatten_plt = plt.imshow(cv_image_flatten)
-        # flatten_plt.format_cursor_data = lambda data : \
-        #      "[{}]".format(hex(data))
+            plt.subplot(223)
+            flatten_plt = plt.imshow(cv_image_flatten)
+            flatten_plt.format_cursor_data = lambda data : \
+                '[{}]'.format(hex(data))
 
-        # plt.subplot(224)
-        # plt.imshow(converted_image)
+            plt.subplot(224)
+            plt.imshow(converted_image)
 
         return z, cluster_weight, len(cv_image_percentage[0]), len(cv_image_percentage)
 
@@ -179,18 +191,20 @@ class ClusterPainter:
 
         from sklearn.cluster import KMeans
         model = KMeans(n_clusters = 2)
-        clusters = model.fit_predict(z, sample_weight = cluster_weight)
+        clusters = model.fit_predict(z) # sample_weight = cluster_weight)
         unique_clusters = np.unique(clusters)
         centers = model.cluster_centers_
 
-        # cluster_plot = plt.subplot(221)
-        # cluster_plot.axes.invert_yaxis()
+        if self.plot_enable:
+            cluster_plot = plt.subplot(221)
+            cluster_plot.axes.invert_yaxis()
         centers_associations = {}
         for cluster in unique_clusters:
             row_index = np.where(clusters == cluster)
             x, y = centers[cluster]
             centers_associations[(x, y)] = z[row_index]
-            # plt.scatter(z[row_index, 0], z[row_index, 1])
+            if self.plot_enable:
+                plt.scatter(z[row_index, 0], z[row_index, 1])
 
         from sklearn.cluster import MeanShift
         center_model = MeanShift()
@@ -208,10 +222,11 @@ class ClusterPainter:
                 else:
                     points = np.append(points, centers_associations[(x, y)], axis = 0)
             main_centers.append([np.mean(points[:, 0]), np.mean(points[:, 1])])
-            # plt.scatter(points[:, 0], points[:, 1])
-            # plt.scatter(centers[row_index, 0], centers[row_index, 1],
-            #            s = 80, marker = 's')
-            # plt.scatter(main_centers[cluster][0], main_centers[cluster][1], marker = 'D')
+            if self.plot_enable:
+                plt.scatter(points[:, 0], points[:, 1])
+                plt.scatter(centers[row_index, 0], centers[row_index, 1],
+                        s = 80, marker = 's')
+                plt.scatter(main_centers[cluster][0], main_centers[cluster][1], marker = 'D')
 
         return main_centers, max_x, max_y
 
@@ -255,9 +270,10 @@ class ClusterPainter:
                         points.append([0, y_zero])
 
                     np_points = np.array(points)
-                    # plt.plot(np_points[:, 0], np_points[:, 1], '--', linewidth = 2)
-
-        # plt.show()
+                    if self.plot_enable:
+                        plt.plot(np_points[:, 0], np_points[:, 1], '--', linewidth = 2)
+        if self.plot_enable:
+            plt.show()
         return top_edge, right_edge, bottom_edge, left_edge
 
     def action(self):
@@ -265,6 +281,9 @@ class ClusterPainter:
             centers, max_x, max_y = self.execute_clustering()
             top_edge, right_edge, bottom_edge, left_edge = self.compute_edge_points(
                 centers, max_x, max_y)
+
+            if self.sim:
+                break
 
             shapes = []
             shapes.append([0, 0])
@@ -294,7 +313,7 @@ class ClusterPainter:
             for coordinates in shapes:
                 x, y = self.transform_cluster_coord(coordinates)
                 pyautogui.moveTo(x, y)
-                print("RECT 1 Moved mouse to: ({}, {})".format(x, y))
+                print('RECT 1 Moved mouse to: ({}, {})'.format(x, y))
                 pyautogui.click()
 
             # Submit entry and click through UI to get next puzzle.
@@ -314,21 +333,29 @@ class ClusterPainter:
                     'ui_targets/submit_button.png', confidence=0.5)
                 time.sleep(1)
 
-        return "Done!"
+        return 'Done!'
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--plot', action = 'store_true',
+        help = "generate analysis plot prior to doing automated action")
+    parser.add_argument('-s', '--simulate', type = str,
+            nargs = 1,
+            help = "simulate analysis of passed in image")
+    args = parser.parse_args()
+
     try:
-        painter = ClusterPainter()
+        painter = ClusterPainter(args.plot, args.simulate)
         painter.set_bounderies()
         painter.set_submit_button()
 
-        input("Press [ENTER] when ready.")
+        input('Press [ENTER] when ready.')
 
         painter.action()
     except pyautogui.FailSafeException:
-        return "PyAutoGUI FailSafe Interrupt"
+        return 'PyAutoGUI FailSafe Interrupt'
 
-if __name__ == "__main__":
-    print("=== CLUSTER PAINTER ===")
-    print("==> Move mouse to top left to stop!")
-    print("Exit Message: ", main())
+if __name__ == '__main__':
+    print('=== CLUSTER PAINTER ===')
+    print('==> Move mouse to top left to stop!')
+    print('Exit Message: ', main())
